ef0ccf0de095750a9c5febdf72990df854763042
luca <savioluca2@gmail.com>
2019-10-26 20:16:41 +0200

    First screen nearly op

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Route, RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjetHighTechComponent } from './projet-high-tech/projet-high-tech.component';
+import { ProjetWebsiteComponent } from './projet-website/projet-website.component';
+
+const routes: Routes = [
+  {path: 'projets-websites' , component: ProjetWebsiteComponent },
+  {path: 'projets-high-tech', component: ProjetHighTechComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjetWebsiteComponent,
+    ProjetHighTechComponent,
+    ProjetHighTechComponent,
+    ProjetWebsiteComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      routes ,
+      {enableTracing: true}
+    ),
     BrowserModule,
     AppRoutingModule
   ],
